591ba1c51beecd40562446ba1a4c83074b7631d5
Jaganaathp3@gmail.com <jaganaath@gmail.com>
2025-10-09 20:49:29 +0530

    jagan's update

--- my-app/my-app-backup/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders landing page heading", () => {
-  render(<App />);
-  const heading = screen.getByText(/Placement Tracker App/i);
-  expect(heading).toBeInTheDocument();
-});
