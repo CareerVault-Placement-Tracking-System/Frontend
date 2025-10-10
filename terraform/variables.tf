d0c6aaa1924a6fd59d0bd974e568fb014f86506b
Jagan-003 <150871619+Jagan-003@users.noreply.github.com>
2025-10-10 07:55:40 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,26 @@
-# Variables used across Terraform files
-
-# Region where Azure resources will be created
 variable "location" {
   type    = string
   default = "southindia"
 }
 
-# Resource group name
 variable "resource_group_name" {
   type    = string
   default = "careervault-rg"
 }
 
-# Prefix for naming resources (keep lowercase, Azure naming rules)
 variable "app_name_prefix" {
   type    = string
   default = "careervault"
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
 }
