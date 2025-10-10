245e15cefcbdfc282b2400e6083a08056151a759
Jagan-003 <150871619+Jagan-003@users.noreply.github.com>
2025-10-10 11:04:30 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,7 @@ output "app_service_url" {
   value       = "https://${azurerm_linux_web_app.app.default_hostname}"
   description = "Public URL of the deployed CAREERVAULT app."
 }
+output "aks_cluster_name" {
+  value       = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the AKS cluster"
+}
